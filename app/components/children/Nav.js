e842b7c03e1586080b8893b4aa403e16f4ba4a8d
Julian Pluas <julianpluas@gmail>
2021-04-01 18:54:52 -0700

    Added a link in the navbar that will take the user to the pick a fruit page

--- app/components/children/Nav.js
+++ app/components/children/Nav.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-require('./nav.css');
+require("./nav.css");
 
 export default class Nav extends Component {
   render() {
@@ -9,38 +9,72 @@ export default class Nav extends Component {
       <nav className="navbar navbar-custom navbar-fixed-top" role="navigation">
         <div className="container nav-container">
           <div className="navbar-header">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                <i className="fa fa-bars"></i>
+            <button
+              type="button"
+              className="navbar-toggle"
+              data-toggle="collapse"
+              data-target=".navbar-main-collapse"
+            >
+              <i className="fa fa-bars"></i>
             </button>
-            <button type="button" className="navbar-toggle navbar-toggle-right" data-toggle="collapse" data-target=".navbar-main-collapse1">
-                <i className="glyphicon glyphicon-plus"></i>
+            <button
+              type="button"
+              className="navbar-toggle navbar-toggle-right"
+              data-toggle="collapse"
+              data-target=".navbar-main-collapse1"
+            >
+              <i className="glyphicon glyphicon-plus"></i>
             </button>
-            <Link to={"/"} ><img className="icon-middle-nav-mobile" src={require("../../img/shapes/shape.png")} /></Link>
+            <Link to={"/"}>
+              <img
+                className="icon-middle-nav-mobile"
+                src={require("../../img/shapes/shape.png")}
+              />
+            </Link>
           </div>
-
-         
-
 
           <div className="collapse navbar-collapse navbar-right navbar-main-collapse1">
-              <ul className="nav navbar-nav">
-                  <li className="hidden">
-                      <a href="#page-top"></a>
-                  </li>
-                  <li>
-                      <a className="page-scroll nav-right-text" href="/trips">
-                        <img className="navigation-icons" src={require("../../img/shapes/offline.png")} />
-                        <p>New Session</p>
-                      </a>
-                  </li>
-                  <li>
-                      <Link to={"/login"} ><div className="page-scroll nav-left-text" data-toggle="modal"><p>Pick a Fruit</p></div></Link>
-                    </li>
-                  <li>
-                      <Link to={"/login"} ><div className="page-scroll nav-left-text" data-toggle="modal"><p>Log-in</p></div></Link>
-                    </li>
-              </ul>
+            <ul className="nav navbar-nav">
+              <li className="hidden">
+                <a href="#page-top"></a>
+              </li>
+              <li>
+                <a className="page-scroll nav-right-text" href="/trips">
+                  <img
+                    className="navigation-icons"
+                    src={require("../../img/shapes/offline.png")}
+                  />
+                  <p>New Session</p>
+                </a>
+              </li>
+              <li>
+                <Link to={"/fruit"}>
+                  <div
+                    className="page-scroll nav-left-text"
+                    data-toggle="modal"
+                  >
+                    <p>Pick a Fruit</p>
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link to={"/login"}>
+                  <div
+                    className="page-scroll nav-left-text"
+                    data-toggle="modal"
+                  >
+                    <p>Log-in</p>
+                  </div>
+                </Link>
+              </li>
+            </ul>
           </div>
-          <Link to={"/"} ><img className="icon-middle-nav-desktop" src={require("../../img/shapes/shape.png")} /></Link>
+          <Link to={"/"}>
+            <img
+              className="icon-middle-nav-desktop"
+              src={require("../../img/shapes/shape.png")}
+            />
+          </Link>
         </div>
       </nav>
     );
