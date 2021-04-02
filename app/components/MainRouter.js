78e69526af9900c46fa36b09ea353d05996591fb
Julian Pluas <julianpluas@gmail>
2021-04-01 18:53:08 -0700

    Imported Fruit component and created a route to return it

--- app/components/MainRouter.js
+++ app/components/MainRouter.js
@@ -1,86 +1,102 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Main";
 import Login from "./Login";
 import Signup from "./Signup";
-import axios from 'axios';
+import Fruit from "./Fruit";
+import axios from "axios";
 
 export default class MainRouter extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      authenticated: false
+      authenticated: false,
     };
   }
 
   authenticate = () => {
     this.setState({
-      authenticated: true
-    })
-  }
+      authenticated: true,
+    });
+  };
 
   deAuthenticate = () => {
     this.setState({
-      authenticated: false
-    })
-  }
+      authenticated: false,
+    });
+  };
 
   logout = () => {
-    axios.get('/apis/users/logout')
-      .then(function (data) {
-        this.deAuthenticate();
-        window.location.reload();
-      }.bind(this)).catch(function (err) {
+    axios
+      .get("/apis/users/logout")
+      .then(
+        function (data) {
+          this.deAuthenticate();
+          window.location.reload();
+        }.bind(this)
+      )
+      .catch(function (err) {
         console.log(err);
       });
-  }
+  };
 
   render() {
     return (
       <Router>
-      	<Switch>
-          <Route exact path="/" render={props => 
-            <Main
-              {...props}
-              authenticate={this.authenticate}
-              deAuthenticate={this.deAuthenticate}
-              authenticated={this.state.authenticated}
-              logout={this.logout}
-            />} 
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <Main
+                {...props}
+                authenticate={this.authenticate}
+                deAuthenticate={this.deAuthenticate}
+                authenticated={this.state.authenticated}
+                logout={this.logout}
+              />
+            )}
           />
-          <Route exact path="/login" render={props => 
-            <Login
-              {...props}
-              authenticate={this.authenticate}
-              deAuthenticate={this.deAuthenticate}
-              authenticated={this.state.authenticated}
-              logout={this.logout}
-            />}
+          <Route
+            exact
+            path="/login"
+            render={(props) => (
+              <Login
+                {...props}
+                authenticate={this.authenticate}
+                deAuthenticate={this.deAuthenticate}
+                authenticated={this.state.authenticated}
+                logout={this.logout}
+              />
+            )}
           />
-          <Route exact path="/signup" render={props => 
-            <Signup
-              {...props}
-              authenticate={this.authenticate}
-              deAuthenticate={this.deAuthenticate}
-              authenticated={this.state.authenticated}
-              logout={this.logout}
-            />} 
+          <Route
+            exact
+            path="/signup"
+            render={(props) => (
+              <Signup
+                {...props}
+                authenticate={this.authenticate}
+                deAuthenticate={this.deAuthenticate}
+                authenticated={this.state.authenticated}
+                logout={this.logout}
+              />
+            )}
           />
-          <Route path="/pricing" render={props => 
-            <Pricing
-              {...props}
-              authenticate={this.authenticate}
-              deAuthenticate={this.deAuthenticate}
-              authenticated={this.state.authenticated}
-              logout={this.logout}
-            />} 
+          <Route
+            path="/fruit"
+            render={(props) => (
+              <Fruit
+                {...props}
+                authenticate={this.authenticate}
+                deAuthenticate={this.deAuthenticate}
+                authenticated={this.state.authenticated}
+                logout={this.logout}
+              />
+            )}
           />
-      	</Switch>
+        </Switch>
       </Router>
     );
   }
