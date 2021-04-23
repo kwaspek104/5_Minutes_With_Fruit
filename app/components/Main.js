6434ab2ee31a6a9a71cb7f2f330041f13968845e
Ocampo <mariaocampo0513@gmail.com>
2021-04-22 23:18:42 -0700

    Added header to Feature page

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,51 +1,44 @@
 // Include React
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import AudioPlayer from './AudioPlayer';
-import Nav from './children/Nav';
-import Footer from './children/Footer'
+// import Nav from "./children/Nav";
+import Footer from "./children/Footer";
+import logo from "./images/orange.png"
 
-require('./main.css');
+require("./main.css");
 
 // Creating the Main component
 export default class Main extends Component {
   render() {
     return (
-      <div className="main">
-        <Nav
+      <div className="container full-height-grow">
+        {/* <Nav
           authenticated={this.props.authenticated}
           authenticate={this.props.authenticate}
           deAuthenticate={this.props.deAuthenticate}
           logout={this.props.logout}
-        />   
-        <header className="intro">
-            <div className="intro-body">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 col-md-offset-2">
-                            <h1 className="brand-heading">6 Minutes With Fruit!</h1>
-                            <h3 className="intro-text">Use Psychology to Hack Your Stress Eating</h3>
-                            
-                            <a href="#about" className="btn btn-circle page-scroll">
-                                <i className="fa down-arrow animated">
-                                </i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        /> */}
+
+        <header className="main-header">
+          <a href="" className="brand-logo">
+            <img className="logo" src={logo} alt=""/>
+            <div className="brand-logo-name">6 Minutes With Fruit</div>
+          </a>
+          <nav className="main-nav">
+            <ul>
+              <li><a href="">About</a></li>
+              <li><a href="">Log In</a></li>
+            </ul>
+          </nav>
         </header>
-        
+        <section className="discover-main-section">
+          <div className="call-to-action">
+            <h1 className="title"></h1>
+          </div>
 
-        <section id="about" className="container content-section text-center">
-            <div className="row">
-                <div className="col-lg-8 col-lg-offset-2">
-                 
-                    <button className="btn" >Start</button>
-                </div>
-            </div>
-        </section>       
+        </section>
 
-        <Footer/>
+        <Footer />
       </div>
     );
   }
