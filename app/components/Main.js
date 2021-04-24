adfa6f044f3bf1246efe5845d729dc30e15247f0
Ocampo <mariaocampo0513@gmail.com>
2021-04-23 21:04:53 -0700

    Completed feature Page

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,9 @@
-// Include React
 import React, { Component } from "react";
-// import AudioPlayer from './AudioPlayer';
 // import Nav from "./children/Nav";
 import Footer from "./children/Footer";
-import logo from "./images/orange.png"
+import logo from "./images/orange.png";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 require("./main.css");
 
@@ -19,35 +19,46 @@ export default class Main extends Component {
           logout={this.props.logout}
         /> */}
 
+        <Helmet>
+          <style>{"body { background-color: rgba(131, 166, 186, 1); }"}</style>
+        </Helmet>
+
         <header className="main-header">
-          {/* <a href="" className="brand-logo">
-            <img className="logo" src={logo} alt=""/>
-            <div className="brand-logo-name">6 Minutes With Fruit</div>
-          </a> */}
+          <a href="" className="brand-logo">
+            <img className="logo" src={logo} alt="" />
+            {/* <div className="brand-logo-name"></div> */}
+          </a>
           <nav className="main-nav">
             <ul>
-              <li><a href="">About</a></li>
-              <li><a href="">Log In</a></li>
+              <li>
+                <Link to={"/About"}>About</Link>
+              </li>
+              <li>
+                <Link to={"/login"}>Log In</Link>
+              </li>
             </ul>
           </nav>
         </header>
 
         <section className="home-main-section">
-          
-
-          <div>
-          <h1 className="title">6 Minutes With Fruit</h1>
-          <span className="subtitle">Use Psychology to Hack Your Stress Eating</span>
-          <a href="#" className="btn">Start</a>
+          <div className="call-to-action">
+            <h1 className="title">6 Minutes With Fruit</h1>
+            <span className="subtitle">
+              Use Psychology to Hack Your Stress Eating
+            </span>
+            <a href="#" className="btn">
+              Start
+            </a>
           </div>
 
           <div className="img-wrapper">
-            <div className="orange-feature-img">
-
-            </div>
+            <div className="orange-feature-img"></div>
           </div>
-          
         </section>
+
+        <div className="home-page-circle-1"></div>
+        <div className="home-page-circle-2"></div>
+        <div className="home-page-circle-3"></div>
 
         <Footer />
       </div>
