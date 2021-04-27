d19deadcfc2376b5fc95b3eca02aff42c6862d70
Ocampo <mariaocampo0513@gmail.com>
2021-04-27 11:57:48 -0700

    updated Instructions section CSS.

--- app/components/Instructions.js
+++ app/components/Instructions.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import logo from "./images/orange.png";
 import { Link } from "react-router-dom";
+
 require("./main.css");
+require("./styles/instructions.css");
 
 // Creating the Main component
 export default class Instructions extends Component {
@@ -10,40 +12,48 @@ export default class Instructions extends Component {
       <div className="container full-height-grow">
         <header className="main-header">
           <a href="" className="brand-logo">
-            <img className="logo" src={logo} alt="" />
-         
+            <img className="logo-secondary" src={logo} alt="" />
           </a>
           <nav className="main-nav">
             <ul>
-              <li>
-                <Link to={"/"}>Homepage</Link>
+              <li className="nav-items secondary-nav">
+                <Link to={"/about"}>About</Link>
               </li>
-             
+              <li className="nav-items secondary-nav">
+                <Link to={"/login"}>Log In</Link>
+              </li>
             </ul>
           </nav>
         </header>
 
-
-        <section className="home-main-section">
-          <div className="call-to-action">
-            
-            <span className="subtitle">
-              Use Psychology to Hack Your Stress Eating
-            </span>
-
-            <Link to="/signup">
-              <button type="button" className="btn">REGISTER</button>
-            </Link>
-
-            
-          </div>
-
-          <div className="img-wrapper">
-            <div className="orange-feature-img"></div>
+        <section className="intro-section">
+          <div className="max-with">
+            <h2 className="title-secondary">Instructions</h2>
+            <div className="carousel">
+              <div className="owl-stage-outer">
+                <div className="owl-stage">
+                  <div className="owl-item">
+                    <div className="card">
+                      <div className="box"></div>
+                    </div>
+                    <div className="card">
+                      <div className="box"></div>
+                    </div>
+                    <div className="card">
+                      <div className="box"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
-       
 
+        <Link to="/signup" className="register">
+          <button type="button" className="btn btn-register">
+            REGISTER
+          </button>
+        </Link>
       </div>
     );
   }
