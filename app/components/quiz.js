58f6020904ff2477eb158af1e6e868bf26acf2db
Ocampo <mariaocampo0513@gmail.com>
2021-04-29 12:00:59 -0700

    Added Navbar to Quiz component

--- app/components/quiz.js
+++ app/components/quiz.js
@@ -1,8 +1,12 @@
 // Include React
 import React, { useState } from "react";
+import logo from "./images/orange.png";
+import { Link } from "react-router-dom";
+import Footer from "./children/Footer";
 
 // Here we include all of the sub-components
 require("./styles/quiz.css");
+require("./main.css");
 
 // Creating the Main component
 export default function Quiz() {
@@ -45,28 +49,50 @@ export default function Quiz() {
     },
   ];
 
+
   return (
-    <div className=''>
-			{/* HINT: replace "false" with logic to display the 
+    <div className="container full-height-grow">
+      <header className="main-header">
+        <a href="" className="brand-logo">
+          <img className="logo-secondary" src={logo} alt="" />
+        </a>
+        <nav className="main-nav">
+          <ul>           
+            <li className="nav-items secondary-nav">
+              <Link to={"/"}>Log Out</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className="">
+        {/* HINT: replace "false" with logic to display the 
       score when the user has answered all the questions */}
-			{false ? (
-				<div className='score-section'>You scored 1 out of {questions.length}</div>
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question 1</span>/{questions.length}
-						</div>
-						<div className='question-text'>This is where the question text should go</div>
-					</div>
-					<div className='answer-section'>
-						<button>Answer 1</button>
-						<button>Answer 2</button>
-						<button>Answer 3</button>
-						<button>Answer 4</button>
-					</div>
-				</>
-			)}
-		</div>
-  )
+        {false ? (
+          <div className="score-section">
+            You scored 1 out of {questions.length}
+          </div>
+        ) : (
+          <>
+            <div className="question-section">
+              <div className="question-count">
+                <span>Question 1</span>/{questions.length}
+              </div>
+              <div className="question-text">
+                This is where the question text should go
+              </div>
+            </div>
+            <div className="answer-section">
+              <button>Answer 1</button>
+              <button>Answer 2</button>
+              <button>Answer 3</button>
+              <button>Answer 4</button>
+            </div>
+          </>
+        )}
+      </div>
+
+      <Footer/>
+    </div>
+  );
 }
